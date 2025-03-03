bdab2398da9c84c45019d26b7821018fc885c787
Marhadi Wijaya <marhadi.wijaya@gmail.com>
2025-03-03 17:21:39 +0700

    Initial commit

---
+++ src/BladeDirectiveServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Sis\BladeContext;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
+
+class BladeDirectiveServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        Blade::directive('controller', function ($controller) {
+            return "<?php /* Controller: {$controller} */ ?>";
+        });
+    }
+
+    public function register()
+    {
+        //
+    }
+}
